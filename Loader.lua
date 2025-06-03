b3c01112c69f6679e7c96df84fedf1dd7ef4682d
Greʧa <138035459+gree4e4ka456@users.noreply.github.com>
2025-06-03 19:05:21 +0300

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/gree4e4ka456/WexortHUB/blob/main/Games/FlingThingsAndPeople"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gree4e4ka456/WexortHUB/refs/heads/main/Games/FlingThingsAndPeople"))()
